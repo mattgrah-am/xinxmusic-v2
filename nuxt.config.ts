029bc6fac18d722537c45068a480db338ef4e951
Matt Graham <mail@matg.dev>
2025-04-04 23:31:43 +1100

    add cloudflare config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/fonts", "@nuxtjs/tailwindcss"],
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxtjs/tailwindcss",
+    "nitro-cloudflare-dev",
+  ],
   nitro: {
     preset: "cloudflare-pages",
 
